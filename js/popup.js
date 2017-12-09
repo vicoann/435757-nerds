ec6ffb075423f184fa15cc89741770dadb538cfa
Anfisa Novikova <anfisa.novikova@tomtom.com>
2017-12-09 14:13:05 +0100

    last version + js and map

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+<script>
+
+  var link=document.querySelector(".write-us-button");
+  var popup=document.querySelector(".write-us-form");
+  var close=document.querySelector(".close-button");
+
+  link.addEventListener ("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("write-us-form-show");
+
+});
+
+  close.addEventListener ("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("write-us-form-show");
+});
+
+</script>
