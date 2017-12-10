cedb7fdcd188a5b0c5ad83e6206812419bbe4598
Anfisa Novikova <anfisa.novikova@tomtom.com>
2017-12-10 19:05:36 +0100

    neskladushki

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,14 @@
-<script>
-
   var link=document.querySelector(".write-us-button");
   var popup=document.querySelector(".write-us-form");
   var close=document.querySelector(".close-button");
 
-  link.addEventListener ("click", function (evt) {
+  link.addEventListener ("click", function (evt){
     evt.preventDefault();
     popup.classList.add("write-us-form-show");
 
 });
 
-  close.addEventListener ("click", function (evt) {
+  close.addEventListener ("click", function (evt){
     evt.preventDefault();
     popup.classList.remove("write-us-form-show");
 });
-
-</script>
